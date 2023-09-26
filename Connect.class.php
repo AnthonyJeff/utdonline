30dbbbd3e252d9c4d16a46c189124c4dbe8e9f34
Anthony Jefferson G. de Moura <anthonyjeff17@gmail.com>
2023-09-26 17:00:49 -0300

    Update Connect.class.php

--- Connect.class.php
+++ Connect.class.php
@@ -11,7 +11,7 @@
 
 		public static function get_instance(){
 			if(!isset(self::$instance)){
-				self::$instance = new PDO('mysql:host=localhost;dbname=agendaonline;', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+				self::$instance = new PDO('mysql:host=localhost;dbname=yourdatabase;', 'user', 'password', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
 			}
